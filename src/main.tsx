11267d3d4ccb94a6e18db21780b08bbca7f74110
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2026-03-25 22:51:09 -0600

    update color theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "@fontsource/geist-sans";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
